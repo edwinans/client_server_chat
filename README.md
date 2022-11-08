c8576a08b56c4ba7a12f5ddd4f9d2feb37a02241
edwinans <edyansari5@gmail.com>
2022-11-08 12:44:52 +0100

    Update .opam, fix accept new connection

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # client_server_chat
-Simple one on one chat
+## Build
+You need to install `lwt` and `logs` modules:  
+`opam install lwt logs`  
+To compile:  
+`dune build`  
+
+## Run
+default ip = localhost  
+default port = 9000  
+To run the server:  
+`dune exec client_server_chat server -- --ip [ip] --port [port]`  
+To run the client:  
+`dune exec client_server_chat client -- --ip [ip] --port [port]`  
+or  
+`_build/default/bin/main.exe server/client --ip [ip] --port [port]`
+
--- lib/client_server.ml
+++ lib/client_server.ml
@@ -1,31 +1,41 @@
 open Lwt
+open Lwt.Syntax
 
 let backlog = 10
 let write_console = Lwt_io.write_line Lwt_io.stdout
 let read_console () = Lwt_io.read_line Lwt_io.stdin
 let last_time = ref 0.
+let connection = ref false
 
 let rec send_message oc =
-  read_console () >>= fun s ->
-  last_time := Unix.gettimeofday ();
-  Lwt_io.write_line oc s >>= fun _ -> send_message oc
+  if !connection then (
+    let* s = read_console () in
+    last_time := Unix.gettimeofday ();
+    Lwt_io.write_line oc s >>= fun _ -> send_message oc)
+  else return_unit
 
 let rec handle_message ic oc side =
   let open Lwt_io in
   read_line_opt ic >>= function
   | Some "ack" ->
       let roundtrip_delay = Unix.gettimeofday () -. !last_time in
-      write_console (Printf.sprintf "[roundtrip delay] : %fms" roundtrip_delay)
-      >>= fun _ -> handle_message ic oc side
+      let* _ =
+        write_console
+          (Printf.sprintf "[roundtrip delay] : %fms" roundtrip_delay)
+      in
+      handle_message ic oc side
   | Some msg ->
       write_console ("[" ^ side ^ "]: " ^ msg) >>= fun _ ->
       write_line oc "ack" >>= fun _ -> handle_message ic oc side
-  | None -> Logs_lwt.info (fun m -> m "Connection closed") >>= return
+  | None ->
+      connection := false;
+      Logs_lwt.info (fun m ->
+          m "Connection closed - Press any key to accept new connections")
 
 let handle_connection ic oc =
   Lwt.on_failure (handle_message ic oc "client") (fun e ->
       Logs.err (fun m -> m "%s" (Printexc.to_string e)));
-  Logs_lwt.info (fun m -> m "New connection") >>= return
+  Logs_lwt.info (fun m -> m "New connection")
 
 let create_socket addr port =
   let open Lwt_unix in
@@ -38,6 +48,7 @@ let create_server sock =
   let rec serve () =
     Lwt_unix.accept sock
     >>= (fun (fd, _) ->
+          connection := true;
           let ic = Lwt_io.of_fd ~mode:Lwt_io.Input fd in
           let oc = Lwt_io.of_fd ~mode:Lwt_io.Output fd in
           handle_connection ic oc <&> send_message oc)
@@ -46,6 +57,7 @@ let create_server sock =
   serve ()
 
 let create_client sock =
+  connection := true;
   let ic = Lwt_io.of_fd ~mode:Lwt_io.Input sock in
   let oc = Lwt_io.of_fd ~mode:Lwt_io.Output sock in
   send_message oc <&> handle_message ic oc "server"
@@ -56,6 +68,6 @@ let connect_server addr port =
   Lwt.on_success
     (Lwt_unix.connect sock @@ ADDR_INET (addr, port))
     (fun _ ->
-      let _ = write_console "Connected to the server" in
+      let _ = write_console "Connecting to the server" in
       ());
   sock
